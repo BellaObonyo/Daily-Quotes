eb95dd7d8b42c1b14e430631bef7670171e1fb65
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-07-16 19:34:39 +0300

    created a quote class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,12 @@
 export class Quotes {
+    constructor(
+        public id: number,
+        public title: string,
+        public author: string,
+        public blogger:string,
+        public dateposted: Date,
+        public upvote: number,
+        public downvote: number,
+    ){}
+    
 }
